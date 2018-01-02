5a5bf1ae5b6b6b98e118868177f07ce102634ad9
Popov Gennadiy <commerce@westtrade.tk>
2018-01-02 22:51:17 +0300

    Some fixes

--- Store.js
+++ Store.js
@@ -5,25 +5,26 @@ function Store(defaultState) {
 }
 
 var storePrototype = {
-    commit(name, payload) {
+    commit: function (name, payload) {
         var count = this.__reducers.length;
         var total = count - 1;
         while (count--) {
-            this.__state = this.__reducers[total - count](
-                Object.assign({}, this.state), name, payload,
-            ) || this.__state;
+            this.__state = (this.__reducers[total - count](Object.assign({}, this.__state), name, payload) 
+                || this.__state);
         }
     },
 
-    use() {
+    use: function () {
         var args = Array.prototype.slice.call(arguments);
         this.__reducers = this.__reducers.concat(args);
     },
-    
-    get state() {
-        return Object.assign({}, this.__state);
-    },
 }
+
+Object.defineProperty(Store.prototype, 'state', {
+    get: function () {
+        return Object.assign({}, this.__state);
+    }
+});
 
 Object.assign(Store.prototype, storePrototype);
 
